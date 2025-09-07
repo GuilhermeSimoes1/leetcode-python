5591017e6e08094ae44d6656c6928de8a8ba4a16
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-09-07 18:47:06 +0100

    1304-Find-N-Unique-Integers-Sum-Up-To-Zero

--- Easy/1304-Find-N-Unique-Integers-Sum-Up-To-Zero/1304-Find-N-Unique-Integers-Sum-Up-To-Zero.py
+++ Easy/1304-Find-N-Unique-Integers-Sum-Up-To-Zero/1304-Find-N-Unique-Integers-Sum-Up-To-Zero.py
@@ -1,45 +1,25 @@
-import random
 from typing import List
-
 
 class Solution:
     def sumZero(self, n: int) -> List[int]:
 
         stack = []
-        lastStack = []
 
         if n == 1:
             return [0]
-        if n % 2 != 0:
 
-            lowerPart = int(n / 2)
-
-            for i in range(0,lowerPart):
-                numRand = random.randint(-1000,-1)
-                stack.append(numRand)
-
-            for y in range(lowerPart,n):
-                if not stack:
-                    break
-                valorPop = stack.pop()
-                lastStack.append(valorPop)
-                valorAdd = valorPop * (-1)
-                lastStack.append(valorAdd)
-            lastStack.append(0)
-            return lastStack
-
+        if n % 2 == 0:
+            for i in range(1,n//2+1):
+                stack.append(i)
+                stack.append(-i)
+            return stack
         else:
-            for i in range(0,n//2):
-                numRand = random.randint(-1000,-1)
-                stack.append(numRand)
-                if stack:
-                    valorPop = stack.pop()
-                    lastStack.append(valorPop)
-                    valorAdd = valorPop * (-1)
-                    lastStack.append(valorAdd)
-            return lastStack
-                    
+            for i in range(1,n//2+1):
+                stack.append(i)
+                stack.append(-i)
+            stack.append(0)
+            return stack
 
 
-sol = Solution()
-print(sol.sumZero(6))
+
+
