2ec91751993487c07762198fa6306950493705f6
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-09-18 00:41:48 +0100

    0070-Climbing-Stairs

---
+++ Easy/0070-Climbing-Stairs/0070-Climbing-Stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n == 0 or n == 1:
+            return 1
+
+        prev1 = 1
+        prev2 = 1
+
+        for _ in range(2, n + 1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        return prev1
+
+
+
+
+
+
