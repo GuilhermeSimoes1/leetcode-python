9bac48167cd59357a0e15b0747db2663cfdea8cc
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-12-17 15:20:15 +0000

    0598-Range-Addition-II

---
+++ Easy/0598-Range-Addition-II/0598-Range-Addition-II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+
+        if not ops:
+            return m * n
+
+        minX = m
+        minY = n
+
+        for x, y in ops:
+            minX = min(minX, x)
+            minY = min(minY, y)
+
+        return minX * minY
+
